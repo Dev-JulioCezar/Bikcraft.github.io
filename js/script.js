2b284e79e499b2d5a189d32e64834ab86568ca29
Julio Cezar <dev.juliocezar@gmail.com>
2023-02-26 13:48:00 -0300

    otimizado para produção

---
+++ js/script.js
@@ -0,0 +1,69 @@
+// Ativar links do Menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLinks(link) {
+  const href = link.href;
+  const url = location.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLinks);
+
+// Ativar itens do Orçamento
+const url = new URLSearchParams(location.search);
+
+function ativar(parametro) {
+  const element = document.getElementById(parametro);
+  if (element) {
+    element.checked = true;
+  }
+};
+
+url.forEach(ativar);
+
+// Perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativar(event) {
+  const qPergunta = event.currentTarget;
+  const controls = qPergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  qPergunta.setAttribute("aria-expanded", ativa);
+}
+
+function ativarP(pergunta) {
+  pergunta.addEventListener("click", ativar)
+}
+
+perguntas.forEach(ativarP);
+
+// Galeria de Bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-img img");
+const galeriaContainer = document.querySelector(".bicicleta-img");
+
+function trocarIMG(event){
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 941px)").matches;
+  if(media){
+  galeriaContainer.prepend(img);
+}
+}
+
+function eventosGaleria(img){
+  img.addEventListener("click", trocarIMG);
+}
+
+galeria.forEach(eventosGaleria);
+
+// ANIMATION
+
+if(window.SimpleAnime){
+  new SimpleAnime();
+}
